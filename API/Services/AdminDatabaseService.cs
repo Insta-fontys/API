0e35eb47038b226503e65a30842fcd56d7b80805
Max <m.neijndorff1999@gmail.com>
2022-01-09 12:09:55 +0100

    Create AdminDatabaseService.cs

---
+++ API/Services/AdminDatabaseService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class AdminDatabaseService
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public async Task<bool> DeleteFan(string name)
+        {
+            IdentityUser user = await userManager.FindByNameAsync(name);
+            try
+            {
+                await userManager.DeleteAsync(user);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
